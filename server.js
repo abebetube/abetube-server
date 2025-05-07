a2511be83684ec8338b5abdf00d06d5297c6d140
abebe <abebe59@gmail.com>
2025-05-07 20:47:36 +0300

    server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const port = process.env.PORT || 3000;
 
-// שליחת abetube.html כשמבקשים את דף הבית
+// הגדרת התיקייה "public" לשירות קבצים סטטיים
+app.use(express.static(path.join(__dirname, 'public')));
+
+// הפניית ברירת מחדל לקובץ abetube.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'abetube.html'));
+  res.sendFile(path.join(__dirname, 'public', 'abetube.html'));
 });
 
 app.listen(port, () => {
-  console.log(`השרת פועל על פורט ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
