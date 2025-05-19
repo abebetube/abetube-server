ee027a0f013563d846c52e1a5706833a1b4fbc6e
abebe <abebe59@gmail.com>
2025-05-20 00:06:41 +0300

    app.py

--- app.py
+++ app.py
@@ -4,12 +4,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/search')
-def search():
-    q = request.args.get('q')
-    if not q:
-        return jsonify({'error': 'No query provided'}), 400
-    return jsonify({'query': q, 'results': ['example1', 'example2']})
+@app.route('/')
+def home():
+    return "Hello from Flask on Render!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
