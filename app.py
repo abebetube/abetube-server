1b98280133baeb7afd8925f7076a5442194e1635
abebe <abebe59@gmail.com>
2025-05-20 00:09:28 +0300

    app.py

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def home():
-    return "Hello from Flask on Render!"
-
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('q')  # מקבל פרמטר בשם q מכתובת URL
+    # תוכל לעבד את הנתון query כאן
+    return jsonify({"result": f"You searched for {query}"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
